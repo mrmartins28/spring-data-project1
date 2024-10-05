f24f640078305d2b2076d46446408ca4740858d1
Leandro Martins <leandroo2828@gmail.com>
2024-10-05 14:53:45 -0300

    Configurado e testado

---
+++ src/test/java/spring_data_projeto1/test/AppSpringDataTest.java
@@ -0,0 +1,17 @@
+package spring_data_projeto1.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
+public class AppSpringDataTest {
+	
+	@Test
+	public void test() {
+		System.out.println("Teste Ok");
+	}
+
+}
